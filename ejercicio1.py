7ddf115b9fbcd2291786aa2f44e584582aa90180
Rwimike <miguerwi@gmail.com>
2025-04-23 21:54:11 -0500

    Update ejercicio1.py

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,41 +1,31 @@
 lista=[]
 cont=True
 proceso=True
-print("Bienvenido a la base de datos")
-while proceso=True:
-    key=int(input("ingrese una de las opciones"))
-    if key == 1:
-        print("hola querido ususario")
-        print("necisitamos tus datos para inciar")
-        while cont:
+id_actual=1
+def agregacion():
+    global id_actual
+    print("hola querido ususario")
+    print("necisitamos tus datos para inciar")
+    while cont:
+        Nom=input("Nombre: ")
+        ape=input("Apellido: ")
+        cor=input("Correo: ")
+        edad=int(input("Edad: "))
+        lista.append([id_actual,Nom,ape,cor,edad])
+        id_actual+=1
+        valor=input("ingrese S o N para continuar o cerrrar: ")
+        if valor=="N"or valor=="n":
+            cont=False
+def lectura():
+    print("Hola usuario quieres ver los datos?")
+    print("Qué dato buscas")
+    dato =input("Ingresa el dato que deseas buscar: ")
 
-            Nom=input("Nombre: ")
-            ape=input("Apellido: ")
-            cor=input("Correo: ")
-            edad=int(input("Edad: "))
-            lista.append([Nom,ape,cor,edad])
-            valor=input("ingrese S o N para continuar o cerrrar: ")
-            if valor=="N"or valor=="n":
-                cont=False
+    for i in range(len(lista)):
+        for j in range(len(lista[i])):
+            if str(lista[i][j]).lower() == dato.lower():
+                print("f\nDato encotrado en la sublista{i},posicion{j}")
+                print(f"Datos al completo:{lista[i]}")
+                return
+    print("\ndato no encontrado")
 
-    elif key==2:
-        
-
-        print("Hola usuario quieres ver los datos?")
-        print("Qué dato buscas")
-        dato =input("Ingresa el dato que deseas buscar: ")
-        encontrado = False
-        for i in range(len(lista)):
-            for j in range(len(lista[i])):
-                if lista[i][j] == dato:
-                    print(f"Encontrado en la sublista {i}, posición {j}")
-                    encontrado = True
-        if not encontrado:
-            print("El valor no está en la lista.")
-
-
-
-    elif key ==3:
-        print("Hola usario qué dato vas a eliminar")
-    elif key==4:
-        print("Hola usuario que dato buscas reemplazar")
